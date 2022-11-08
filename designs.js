a7c65cd3c3ea95ae9a27008dd5ae2c849a5c26f4
KendraAKing <kendra.a.mcdonnell@gmail.com>
2022-11-07 20:27:43 -0500

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,44 +3,22 @@ var chooseColor = document.getElementById('colorPicker');
 // Select size input using name from html code
 var chooseSize = document.getElementById('sizePicker');
 // When size is submitted by the user, call makeGrid()
-chooseSize.addEventListener("submit", function(event)
-{
-    event.preventDefault();
-    clearGrid();
-    makeGrid();    
-});
-// When size is submitted by the user, call makeGrid()
+
 var chooserows = document.getElementById('inputHeight');
 var choosecolumns = document.getElementById('inputWidth');
-var artCanvas = document.getElementById('pixelCanvas');
-function makeGrid() 
+var pxCanvas = document.getElementById('pixelCanvas');
+chooseSize.addEventListener("submit", function(event) {
+    pxCanvas.innerHTML = "";
+    event.preventDefault();
+    makeGrid();    
+});
+function makeGrid()
 {
 // Your code goes here!
-    for (r = 0; r <= chooserows.value; r++)
-    {
-        var row = artCanvas.insertRow(r);
-        for (c=0; c <= choosecolumns.value; c++)
-        {   
+    for (r = 0; r <= chooserows.value; r++) {
+        var row = pxCanvas.insertRow(r);
+        for (c = 0; c <= choosecolumns.value; c++) {
             var column = row.insertCell(c);
-            column.addEventListener("click", function(event) {
-                event.preventDefault();
-                column.style.backgroundColor = chooseColor.value; 
-            });
+            };
         }
     }   
-}
-function clearGrid()
-{
-    for (r = 0; r <= chooserows.value; r++)
-    {
-        var row = artCanvas.insertRow(r);
-        for (c=0; c <= choosecolumns.value; c++)
-        {
-            var column = row.insertCell(c);
-            column.addEventListener("click",function(event) {
-                event.preventDefault();
-                column.style.backgroundColor = 'white';
-            });
-        }
-    }       
-}
